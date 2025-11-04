4b160cc6507bb4b8a7e7bdc18ce53886b5884038
junaidksamad <junaidksamad@gmail.com>
2025-11-04 12:56:59 +0530

    added

---
+++ Asp.NetCore/2.C#/junaidh/machine test/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Xml.Serialization;
+
+struct Patient
+{
+    public int ID;
+    public string Name;
+    public int Age;
+}
+
+internal class program
+{
+    static void Main()
+    {
+        Patient[] patients = new Patient[5]; 
+
+        Console.WriteLine("Enter details for 5 patients:\n");
+
+        
+        for (int i = 0; i <= patients.Length; i++)
+        {
+            Console.WriteLine($"Enter details for Patient {i + 1}:");
+
+            Console.Write("ID: ");
+            patients[i].ID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Name: ");
+            patients[i].Name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            patients[i].Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(); 
+        }
+
+       
+        Console.WriteLine("--- Patient Details ---");
+        for (int i = 0; i <=patients.Length; i++)
+        {
+            Console.WriteLine($"Patient {i + 1}:");
+            Console.WriteLine($"ID: {patients[i].ID}");
+            Console.WriteLine($"Name: {patients[i].Name}");
+            Console.WriteLine($"Age: {patients[i].Age}");
+            
+        }
+    }
+}
+
+//    static Patient[] Patients = new Patient[5];
+//        static Patient patients; 
+
+//    public static void Main(string[] args)
+//    {
+//        do
+//        { 
+//        Console.WriteLine("Hospital Management System");
+//        Console.WriteLine(" 1: register ");
+//        Console.WriteLine(" 2: login ");
+
+//        string ch =Console.ReadLine();
+       
+
+//            switch (ch)
+//            {
+//                case "1":
+//                    register();
+//                    break;
+//                case "2":
+//                    login();
+//                    break;
+//                default:
+//                    Console.WriteLine("invalid choice");
+//                    break;
+//            }
+//            Console.WriteLine(" continue (Y/N)");
+//        }
+//        while (choice == "Y" || choice == "y");
+
+//    }
+    
+
+
+//    public void register()
+//    {
+//        Patient hos = new Patient();
+//        Console.WriteLine("Patient ID");
+//        hos.Id = Convert.ToInt32(Console.ReadLine);
+//        Console.WriteLine("Patient name");
+//        hos.Name = Console.ReadLine();
+//        Console.WriteLine("Patient age");
+//        hos.age = Convert.ToInt32(Console.ReadLine());
+//    }
+
+//    public void login()
+//    {
+//        Console.WriteLine("Id");
+//        int Id = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Name");
+//        string Name = Console.ReadLine();
+
+//        bool logginsuccess=false;
+//        for (int i = 0; i <= Patients.Length; i++)
+//        {
+//            if (Patients[i].Id == Id && Patients[i].Name == Name)
+//            {
+//                Console.WriteLine("login Successfull");
+//                bool loginsuccess = true;
+//                patients = Patients[i];
+
+//                patientsdetails();
+
+//            }
+//            else
+//            {
+//                Console.WriteLine("Login error");
+//            }
+                
+//        }
+       
+//    }
+//    public void patientsdetails()
+//    {
+
+//        do
+//        {
+//            Console.WriteLine(" 1:list");
+//            Console.WriteLine("display");
+//        }
+//        while(true);
+
+//    }
+//    public void list()
+//    {
+//        Console.WriteLine("Id :11 , Name :hary ,age :20");
+//        Console.WriteLine("Id :12, name : rahul,age :55");
+//        Console.WriteLine("Id :13, Name :wale , age :54");
+//        Console.WriteLine("Id : 14, name : gwen, age :67");
+//        Console.WriteLine("Id :15 , name :ravi , age :34");
+//    }
+//    public void display()
+//    {
+//        Console.WriteLine($"{patients.Id}");
+//        Console.WriteLine($"{patients.Name}");
+//        Console.WriteLine($"{patients.age}");
+//    }
+//}
+
---
+++ Asp.NetCore/2.C#/junaidh/workshop4/workshop 4/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace provider_app
+{
+    public class program
+    {
+        struct jobProvider
+        {
+            public string CompanyName;
+            public string Email;
+            public string Phone;
+            public string Password;
+            public string JobRole;
+            public string Salary;
+            public int ExperienceRequired;
+        }
+        static jobProvider[] JobProvider = new jobProvider[1];
+        static jobProvider loggedinUser;
+        static int userCount = 0;
+        static void Main (string[] args)
+        {
+            string ch;
+            do
+            {
+                Console.WriteLine("***********************************");
+                Console.WriteLine("choose the option you want to from below");
+                Console.WriteLine("1 : Login");
+                Console.WriteLine("2 : Register as a new job provider");
+                Console.WriteLine("***********************************");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Login();
+                        break;
+
+                    case "2":
+                        Register();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue (Y/N)");
+                ch = Console.ReadLine();
+            }
+            while (ch == "y" || ch == "Y");
+
+        }
+
+        static void Login()
+        {
+            Console.WriteLine("********************************");
+            Console.WriteLine();
+            Console.WriteLine("Enter Email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            string password = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("*********************************");
+
+            bool loginSuccessfull = false;
+
+            foreach (jobProvider jp in JobProvider)
+            {
+                if (jp.Email == email && jp.Password == password) ;
+                {
+                    loginSuccessfull = true;
+                    loggedinUser = jp;
+
+                    Console.WriteLine("Login Successfull");
+                    Console.WriteLine();
+                    Console.WriteLine("Here are your details :");
+                    Console.WriteLine("CompanyName :{0}", loggedinUser.CompanyName);
+                    Console.WriteLine("Email :{0}", loggedinUser.Email);
+                    Console.WriteLine("Password :{0}", loggedinUser.Password);
+                    Console.WriteLine("Salary :{0}", loggedinUser.Salary);
+                    Console.WriteLine("ExperienceRequired :{0}", loggedinUser.ExperienceRequired);
+                    Console.WriteLine("JobRole :{0}", loggedinUser.JobRole);
+                    Console.WriteLine("Phone :{0}", loggedinUser.Phone);
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Register()
+        {
+            jobProvider jobProvider = new jobProvider ();
+
+            Console.WriteLine("***************************************");
+            Console.WriteLine();
+            Console.WriteLine("Enter companyName :");
+            jobProvider.CompanyName = Console.ReadLine();
+
+            Console.WriteLine("enter Email :");
+            jobProvider.CompanyName = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone :");
+            jobProvider.Phone = Console.ReadLine();
+
+            Console.WriteLine("Enter Password :");
+            jobProvider.Password = Console.ReadLine();
+
+            Console.WriteLine("Enter JobRole :");
+            jobProvider.JobRole = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary :");
+            jobProvider.Salary = Console.ReadLine();
+
+            Console.WriteLine(" Enter ExperienceRequired :");
+            jobProvider.ExperienceRequired = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("**************************************");
+
+            JobProvider[userCount] = jobProvider;
+            userCount++;
+        }
+    }
+}
