02be42356fdbca7be7bf75cb405b79b283185a6f
Edwin9762 <edwinbaby9762@gmail.com>
2025-11-06 09:29:05 +0530

    added

--- Asp.NetCore/2.C#/Workshop/Workshop-4/Provider_App/Provider_App/obj/Debug/net8.0/Provider_App.AssemblyInfo.cs
+++ Asp.NetCore/2.C#/Workshop/Workshop-4/Provider_App/Provider_App/obj/Debug/net8.0/Provider_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5363e92f3f869613dd3011b6fffda6845ead7be6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985dea28df2a7f333f59410d2a057a55920a3f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
