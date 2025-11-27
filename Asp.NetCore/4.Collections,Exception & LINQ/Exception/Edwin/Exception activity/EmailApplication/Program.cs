0b069bd0f0a45c4a7de208fd15779aaec993f026
Edwin9762 <edwinbaby9762@gmail.com>
2025-11-27 09:59:58 +0530

    added

---
+++ Asp.NetCore/4.Collections,Exception & LINQ/Exception/Edwin/Exception activity/EmailApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using EmailApplication;
+using EmailApplication.Exception;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter Email:");
+            string email = Console.ReadLine();
+            EmailValidator.ValidateEmail(email);
+            Console.WriteLine("Valid email.");
+        }
+        catch (InvalidEmailException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
