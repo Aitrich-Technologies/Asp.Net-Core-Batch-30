2b7b217d4c0d8a43ddec300c8ea0b89e753f82db
Edwin9762 <edwinbaby9762@gmail.com>
2025-12-08 11:48:25 +0530

    added

---
+++ Asp.NetCore/4.Collections,Exception & LINQ/edwin Machinetest/movie/movie/Exceptions/MovieNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movie.Exceptions
+{
+    public class MovieNotFoundException : Exception
+    {
+        public MovieNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ Asp.NetCore/4.Collections,Exception & LINQ/edwin Machinetest/movie/movie/Models/Moviee.cs
@@ -0,0 +1,46 @@
+﻿
+using movie.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movie.Models
+{
+    public class Moviee
+    {
+
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int AvailableSeats { get; set; }
+
+        public Moviee(int movieId, string title, string genre, int seats)
+        {
+            MovieId = movieId;
+            Title = title;
+            Genre = genre;
+            AvailableSeats = seats;
+        }
+
+        public void BookTickets(int count)
+        {
+            if (count > AvailableSeats)
+            {
+                throw new SeatNotAvailableException("Not enough seats available!");
+            }
+            AvailableSeats -= count;
+        }
+
+        public void CancelTickets(int count)
+        {
+            AvailableSeats += count;
+        }
+
+        public override string ToString()
+        {
+            return $"[{MovieId}] {Title} | Genre: {Genre} | Seats: {AvailableSeats}";
+        }
+    }
+}
