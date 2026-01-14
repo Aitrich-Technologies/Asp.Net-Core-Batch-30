f540165c2e17209d8428695b6707a60dc28c7a11
jj662 <joyaljohnson723@gmail.com>
2026-01-14 09:56:54 +0530

    added

---
+++ Asp.NetCore/4.Collections,Exception & LINQ/Exception/Edwin/Exception activity/Application/Exception/InvalidEmailException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exception
+{
+    internal class InvalidEmailException:EmailValidator
+    {
+        public InvalidEmailException(string message) : base(message) { }
+    }
+}
